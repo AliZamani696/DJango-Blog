845a4821245aee1d783d263cf5606889b50205f9
ali zamani <azamani081@gmail.com>
2025-11-24 10:47:53 +0330

    complate the  administration features

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,68 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = [
+        'username',
+          'email', ''
+          'is_staff',
+          'is_active',
+          "is_superuser"
+          ]
+    list_filter = [
+        'is_staff',
+        'is_active'
+        ]
+    search_fields = (
+        'username',
+        'email'
+        )
+    ordering = ('email',)
+    fieldsets = (
+        ("authentication", {
+            'fields':(
+                'username',
+                'email',
+                'password'
+                )
+            }),
+        ('Permissions',{
+            'fields':(
+                'is_staff',
+                'is_active',
+                'is_superuser'
+                )
+            }),
+           ( "group permissions",{
+                'fields':(
+                    "groups",
+                    "user_permissions"
+                    )
+            }),
+            ("importent dates",{
+                'fields':(
+                    "last_login",
+                )
+            })
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                        'username',
+                        'email',
+                        'password1',
+                        'password2',
+                        'is_staff',
+                        'is_active'
+                           )}
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
